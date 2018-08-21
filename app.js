1030294b7a37eb8affae87aea03d4695d7b4afbf
Bibin Tom <bibintomj@gmail.com>
2018-08-21 10:34:09 +0530

    Committing all files. Setup Google Strategy

--- app.js
+++ app.js
@@ -1,20 +1,39 @@
-const express = require('express');
-const authRoutes = require('./routes/auth-routes');
-const passportSetup = require('./config/passport-setup');
+const express = require('express')
+const authRoutes = require('./routes/auth-routes')
+const profileRoutes = require('./routes/profile-routes')
+const passportSetup = require('./config/passport-setup')
+const mongoose = require('mongoose')
+const keys = require('./config/keys')
+const cookieSession = require('cookie-session')
+const passport = require('passport')
 
-const app = express();
+const app = express()
 
 // set view engine
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
+
+app.use(cookieSession({
+  maxAge: 24 * 60 * 60 * 1000,
+  keys: [keys.session.cookieKey]
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+// connect to MongoDB
+mongoose.connect(keys.mongodb.dbURI, { useNewUrlParser: true }, () => {
+  console.log('ABCD ')
+})
 
 // set up routes
-app.use('/auth', authRoutes);
+app.use('/auth', authRoutes)
+app.use('/profile', profileRoutes)
 
 // create home route
 app.get('/', (req, res) => {
-    res.render('home');
-});
+  res.render('home')
+})
 
 app.listen(3000, () => {
-    console.log('app now listening for requests on port 3000');
-});
+  console.log('app now listening for requests on port 3000')
+})
--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,14 @@
 // add this file to .gitignore
 
 module.exports = {
-        google: {
-            clientID: '616581710231-3gh4r5mnbeadi0r9h1gbvk844kbir61o.apps.googleusercontent.com',
-            clientSecret: 'y3kKg0rZlk3WhE5u-A5aHwsD'
-        }
-};
+  google: {
+    clientID: '493935173681-flaalk562s05q1c30hb6gsa1cqjb80ha.apps.googleusercontent.com',
+    clientSecret: 'KBuWk2kEvsAhYXrUAkbaPwCo'
+  },
+  mongodb: {
+    dbURI: 'mongodb://root:Pass%40123@ds125482.mlab.com:25482/oauth-rbac-test'
+  },
+  session: {
+    cookieKey: 'GoFishermenKeralaFlood2018'
+  }
+}
